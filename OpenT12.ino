8d3a3f1478cccf081edcbe2103427105f14cb0c8
createskyblue <createskyblue@outlook.com>
2021-07-31 22:52:38 +0800

    好用的编码器模块

--- OpenT12.ino
+++ OpenT12.ino
@@ -2,23 +2,19 @@
 #include <Arduino.h>
 #include <Ticker.h>
 #include <U8g2lib.h>
-#include "ESPRotary.h";
+#include "Type.h"
+#include "Rotary.h"
 
 /////////////////////////////////////////////////////////////////
-#define ROTARY_PIN1	0
-#define ROTARY_PIN2	2
-
-#define CLICKS_PER_STEP   4   // this number depends on your rotary encoder 
-#define MIN_POS   5
-#define MAX_POS   20
 
 
+#define BEEP_PIN    14
+
+#define RotaryCoolTime 3
+uint32_t RotaryCoolTimeTimer = 0;
 /////////////////////////////////////////////////////////////////
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C Disp(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
-ESPRotary r = ESPRotary(ROTARY_PIN1, ROTARY_PIN2, CLICKS_PER_STEP, MIN_POS, MAX_POS);
-Ticker ESPRotaryLoop;
 /////////////////////////////////////////////////////////////////
-int counter = 0;
 uint32_t DispFlashTimer = 0;
 void ESPRotaryInterrupt();
 
@@ -33,30 +29,25 @@ void setup() {
     Disp.setFontPosTop();
     Disp.setFont(u8g2_font_wqy12_t_gb2312);
 
+    //初始化编码器
+    sys_RotaryInit();
+
     //初始化GPIO
-    pinMode(14, OUTPUT);
+    pinMode(BEEP_PIN, OUTPUT);
 
     //初始化中断
-    ESPRotaryLoop.attach_ms(10, ESPRotaryInterrupt);
-
-    Serial.println("\n\Ranged Counter");
-    Serial.println("You can only set values between " + String(MIN_POS) + " and " + String(MAX_POS) + ".");
-
-    r.setChangedHandler(rotate);
-    r.setLeftRotationHandler(showDirection);
-    r.setRightRotationHandler(showDirection);
+    //ESPRotaryLoop.attach_ms(10, ESPRotaryInterrupt);
 
 }
 
 char buffer[50];
 void loop() {
 
-    //r.loop();
     if (millis() - DispFlashTimer > 33) {
         //tone(14, 1000);
 
         Disp.clearBuffer();
-        sprintf(buffer, "计数:%d", counter);
+        sprintf(buffer, "计数:%f", sys_Counter_Get());
         Disp.setCursor(0, 1);
         Disp.print(buffer);
 
@@ -71,21 +62,3 @@ void loop() {
     }
 
 }
-
-/////////////////////////////////////////////////////////////////
-
-// on change
-void rotate(ESPRotary& r) {
-    counter = r.getPosition();
-    Serial.println(counter);
-}
-
-// on left or right rotation
-void showDirection(ESPRotary& r) {
-    Serial.println(r.directionToString(r.getDirection()));
-}
-
-void ICACHE_RAM_ATTR ESPRotaryInterrupt() {
-    r.loop();
-}
-/////////////////////////////////////////////////////////////////
--- Rotary.h
+++ Rotary.h
@@ -1,9 +1,16 @@
 #ifndef sys_Counter_H
 #define sys_Counter_H
-#include <stdint.h>
-void sys_Counter_Set(float min, float max, float step, float c);
-void sys_Counter_Add(void);
-void sys_Counter_Less(void);
-float sys_Counter_Get(void);
+#include <c_types.h>
+#include "Type.h"
+
+#define ROTARY_PIN1	D3
+#define ROTARY_PIN2	D4
+
+#define ROTARY_TYPE 2
+
+void sys_RotaryInit(void);
+void sys_Counter_Set(double min, double max, double step, double c);
+void sys_Counter_IRQHandler(void);
+double sys_Counter_Get(void);
 uint8_t sys_KeyProcess(void);
 #endif
