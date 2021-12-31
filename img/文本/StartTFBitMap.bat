783bdc04cb8e2882b760289c78b1735127a50175
createskyblue <createskyblue@outlook.com>
2021-12-31 13:38:19 +0800

    迁移至 PlatformIO

--- img/文本/StartTFBitMap.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-for %%s in (*.png) do (
-echo %%s
-start.exe %%s %%s f>>bitmaps.h
-
-)
-
-pause
--- serial_putc.cpp
+++
@@ -1,7 +0,0 @@
-#include "OpenT12.h"
-int fputc(int c, FILE* f)
-{
-    Serial.write(c);
-    //SerialBT.write(c);
-    return c;
-}
