bf61e9d21e6f3d9198e679eafdecb14b3e681775
createskyblue <createskyblue@outlook.com>
2022-03-05 23:14:58 +0800

    First

---
+++ README.md
@@ -0,0 +1,28 @@
+# Easy U8g2 font generate tools
+
+这是一个易于操作的U8g2字体生成工具，使用者只需要输入需要转换的文本，选择字体文件以及设置字体大小，该工具会自动对文本进行去重、生成map字体映射文件、生成bdf字库并最终导出C语言字库代码
+
+## 实现功能
+
+- [X] 自定义文本
+- [X] 文本自动去重
+- [X] ASCII字符可选
+- [X] 自定义ttf字体
+- [X] 自定义字体大小
+
+## 使用方式
+
+    python3 main.py
+
+## 第三方工具使用
+
+| 名称 | 来源 |
+| :----: | :----:  |
+| bdfconv | <https://github.com/olikraus/u8g2> |
+| otf2bdf | <http://sofia.nmsu.edu/~mleisher/Software/otf2bdf/> |
+| Alibaba-PuHuiTi-Medium | https://ics.alibaba.com/project/Hn8mXx |
+
+## 演示图片
+![shell界面](shell.jpg)
+![生成字体](font_tga.jpg)
+![实际lcd演示](hc32f460_lcd.jpg)
---
+++ main.py
@@ -0,0 +1,92 @@
+import os
+#确保win10终端颜色正常显示
+if os.name == "nt":
+    os.system("")
+
+#字符转unicode函数
+def to_unicode(string):
+    ret = ""
+    for v in string:
+        ret = ret + hex(ord(v)).upper().replace('0X', '$')
+    return ret
+
+#启动信息
+print('\033[1;31;43m',"{:=^40}".format("U8g2字库生成器 V1.0"),'\033[0m')
+print('\033[1;30;33m',"Email: createskyblue@outlook.com\n",'\033[0m')
+
+#获取字符输入 电子科技大学中山学院
+inputText = input('\033[1;31;43m[ASK] 请输入需要生成字库的文本（自动去重）：\033[0m')
+# inputText = " 电子科技大学中山学院"
+
+#获取生成目标字体名称
+targetFontName = input("\033[1;30;33m[ASK] 生成字库命名 >\033[0m")
+# targetFontName = "testFont"
+
+#获取字体文件
+ttfFontPath = input("\033[1;30;33m[ASK] 输入ttf格式字体路径 >\033[0m")
+tftFontName = os.path.basename(ttfFontPath)
+tftFontNameReal = tftFontName.split('.')[0]
+# ttfFontPath = "font/MFLanSong_Noncommercial-Regular.ttf"
+
+#获取字体pix大小
+fontSize = input("\033[1;30;33m[ASK] 输入生成字库字体大小(pix像素) >\033[0m")
+# fontSize = 14
+
+#询问：输入是否过滤ascii
+del_ASCII_flag  = False
+del_ASCII_flag_input = input("\033[1;30;33m[ASK] 是否过滤ASCII? (Y/n) >\033[0m")
+if (del_ASCII_flag_input == "y" or del_ASCII_flag_input == "Y" or del_ASCII_flag_input == ""):
+    del_ASCII_flag = True
+
+#询问：map字符映射文件是否包含ASCII
+add_ASCII_flag = False
+add_ASCII_flag_input = input("\033[1;30;33m[ASK] MAP字符映射文件是否包含ASCII? (Y/n) >\033[0m")
+if (add_ASCII_flag_input == "y" or add_ASCII_flag_input == "Y" or add_ASCII_flag_input == ""):
+    add_ASCII_flag = True
+
+#创建原子字典，对目标字去重
+atomText = set(inputText)
+print("原子字库：",atomText)
+
+#原子字典转unicode格式
+unicodeText = []
+for c in atomText:
+    #选择过滤ASCII结果
+    if (del_ASCII_flag and ord(c) < 255):
+        continue
+    #unicode转换
+    unicodeText.append(to_unicode(c))
+#对转换结果进行排序
+unicodeText.sort()
+print("转unicode原子字库：",unicodeText)
+
+#生成map字符映射表
+mapFileDatas = ""
+#选择性加入ASCII到map字符映射表
+if (add_ASCII_flag):
+    mapFileDatas += "32-128"
+#合并unicode字典到映射表
+for u in unicodeText:
+    if (len(mapFileDatas) > 0):
+        mapFileDatas += ", " #分隔符：这里必须要有空格
+    mapFileDatas += u
+print("字符map映射表：",mapFileDatas)
+
+#覆盖创建map文件
+mapPath = "map/{}.map".format(targetFontName)
+mapFile_f = open(mapPath,"w")
+mapFile_f.write(mapFileDatas)
+mapFile_f.close()
+
+#生成bdf字库
+bdfPath = "bdf/{0}_{1}.bdf".format(tftFontNameReal,fontSize)
+c_codePath = "code/{0}.c".format(targetFontName)
+otf2bdfCMD = "otf2bdf.exe -r 100 -p {0} -o {1} {2}".format(fontSize,bdfPath,ttfFontPath)
+print(">",otf2bdfCMD)
+os.system(otf2bdfCMD)
+#生成u8g2目标C语言字库文件
+bdfconvCMD = "bdfconv.exe -v -b 2 -f 1 {0} -M {1} -n {2} -o {3} -d {0}".format(bdfPath,mapPath,targetFontName,c_codePath)
+print(">",bdfconvCMD)
+os.system(bdfconvCMD)
+#完成信息
+print('\033[1;37;42m',"[操作完成]",'\033[0m')
