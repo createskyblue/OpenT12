5c945cca34e49cbde415abba3084098d344bd199
createskyblue <createskyblue@outlook.com>
2022-03-12 15:17:40 +0800

    取消默认静音

--- src/main.cpp
+++ src/main.cpp
@@ -62,7 +62,7 @@ uint8_t SmoothAnimation_Flag        = true;
 float   ScreenBrightness            = 128;
 uint8_t OptionStripFixedLength_Flag = false;
 
-uint8_t Volume = 0;
+uint8_t Volume = 100;
 uint8_t RotaryDirection = false;
 uint8_t HandleTrigger = HANDLETRIGGER_VibrationSwitch;
 
